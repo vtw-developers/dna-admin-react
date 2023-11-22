6a25bae9ea6702ad6f02787f8f88abb74b66ed53
임병렬 <byeongryeol.im@vtw.co.kr>
2023-11-22 16:37:53 +0900

    operation tree tab 추가

--- packages/react/src/pages/dna/ops/operation-cards/operation-cards.tsx
+++ packages/react/src/pages/dna/ops/operation-cards/operation-cards.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-// import TabPanel, { Item as TabPanelItem } from 'devextreme-react/tab-panel';
+import TabPanel, { Item as TabPanelItem } from 'devextreme-react/tab-panel';
 
 export const OperationCards = () => {
   return (
     <div className='dx-card details-card' style={{ height: '700px' }}>
-      {/*<TabPanel showNavButtons deferRendering={false}>
-        <TabPanelItem title='Monitoring1'>
+      <TabPanel showNavButtons deferRendering={false}>
+        <TabPanelItem title='플로우'>
           <iframe src='' style={{ height: '100%', width: '100%', border: 'none' }} />
         </TabPanelItem>
-        <TabPanelItem title='Monitoring2'>
+        <TabPanelItem title='데이터소스'>
           <iframe src='' style={{ height: '100%', width: '100%', border: 'none' }} />
         </TabPanelItem>
-      </TabPanel>*/}
-      <iframe src='' style={{ height: '100%', width: '100%', border: 'none' }} />
+        <TabPanelItem title='모니터링'>
+          <iframe src='' style={{ height: '100%', width: '100%', border: 'none' }} />
+        </TabPanelItem>
+      </TabPanel>
+      {/*<iframe src='' style={{ height: '100%', width: '100%', border: 'none' }} />*/}
     </div>
   );
 };
